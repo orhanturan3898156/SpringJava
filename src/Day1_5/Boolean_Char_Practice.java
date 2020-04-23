a31e232091233388cccc1604a64f0b45072b8260
orhanturan3898156 <orhanturan1566@gmail.com>
2020-04-22 22:44:25 -0400

    commit

---
+++ src/Day1_5/Boolean_Char_Practice.java
@@ -0,0 +1,28 @@
+package Day1_5;
+
+
+    public class Boolean_Char_Practice {
+
+        public static void main(String[] args) {
+
+            int a='a' + 'b'; //97+98 //you cant use byte because it is very small
+            System.out.println(a);
+            boolean r1='a'==97;
+            boolean r2='b'==98;
+            boolean r3= 97<98;
+            System.out.println(r1);
+            System.out.println(r3);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
