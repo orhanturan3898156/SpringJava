7a3f9058e4fef2012da3bb82e72f8bf98c0e83e8
orhanturan3898156 <orhanturan1566@gmail.com>
2020-04-23 12:51:12 -0400

    commit

--- src/Day27_DateTime/Dates_Formatting.java
+++ src/Day27_DateTime/Dates_Formatting.java
@@ -9,6 +9,7 @@ public class Dates_Formatting {
         year :yy,yyyy
         month:MM,MMM
         day  :dd
+        days name: EEE(first three letter of the day)
          */
 
         DateTimeFormatter dtf=DateTimeFormatter.ofPattern("MM/dd/yyyy"); //you can change its order how ever like
@@ -17,6 +18,23 @@ public class Dates_Formatting {
 
         LocalDate date1=LocalDate.now();
         System.out.println(date1.format(dtf));
+
+        System.out.println("======================================");
+
+        DateTimeFormatter dtf2=DateTimeFormatter.ofPattern("MMM/dd/yy");
+        LocalDate date3=LocalDate.of(2020,04,10);
+        String str1=date3.format(dtf2);
+        System.out.println(str1);
+
+        LocalDate date2=LocalDate.now();
+        System.out.println(date2.format(dtf2));
+
+        System.out.println("======================================");
+
+        DateTimeFormatter dtf3=DateTimeFormatter.ofPattern("MMM/dd/yy EEE");
+        ///we will say EEE MM/dd/yy /onune yada sonuna yazabilirsin. for full name EEEE (4 E)
+        LocalDate date4=LocalDate.now();
+        System.out.println(date4.format(dtf3));
 
 
 
