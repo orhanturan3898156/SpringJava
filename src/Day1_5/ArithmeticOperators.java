9fa433b6894903a0a56cbbe6a7c85eef34917b21
orhanturan3898156 <orhanturan1566@gmail.com>
2020-04-22 22:41:49 -0400

    commit

---
+++ src/Day1_5/ArithmeticOperators.java
@@ -0,0 +1,16 @@
+package Day1_5;
+
+
+    public class ArithmeticOperators {
+        public static void main(String[] args) {
+
+            System.out.println(5 + 4);
+            System.out.println(10 % 4);
+            System.out.println(10 % 3);
+            System.out.println(7 % 2);
+            System.out.println(11 % 3);
+
+
+        }
+
+    }
