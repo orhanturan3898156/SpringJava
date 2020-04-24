c806c7905068d7fda345992513ffd4d496423372
orhanturan3898156 <orhanturan1566@gmail.com>
2020-04-24 10:58:48 -0400

    Time_Formatting

---
+++ src/Day28_Day_and_Time/TIME.java
@@ -0,0 +1,30 @@
+package Day28_Day_and_Time;
+
+
+import java.time.LocalTime;
+
+public class TIME {
+    public static void main(String[] args) {
+
+        //by default it is 24 hour format, we will learn how to format it like date;
+        LocalTime time1 = LocalTime.now();
+        System.out.println(time1);
+
+        LocalTime time2 = LocalTime.of(15, 30, 45);
+        System.out.println(time2);
+
+
+        /*
+        Time Formatting;
+
+        -hour    :hh
+        -minute  :mm
+        -second  :ss
+        -am/pm   : a >>>just a
+
+
+
+         */
+
+    }
+}
---
+++ src/Day28_Day_and_Time/Time_Formatting.java
@@ -0,0 +1,22 @@
+package Day28_Day_and_Time;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time_Formatting {
+    public static void main(String[] args) {
+
+
+        DateTimeFormatter dtf=DateTimeFormatter.ofPattern("hh:mm a");
+        LocalTime time1=LocalTime.of(16,30);
+        System.out.println(time1.format(dtf));
+
+        LocalTime time2=LocalTime.of(12,0);
+        System.out.println(time2.format(dtf));
+
+        boolean result1=time1.isAfter(time2);
+        System.out.println(result1);
+
+
+    }
+}
