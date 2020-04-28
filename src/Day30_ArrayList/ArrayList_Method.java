19525e9a999bc7198124f50d8035e72e6d4a983a
orhanturan3898156 <orhanturan1566@gmail.com>
2020-04-28 14:39:21 -0400

    commit

--- src/Day30_ArrayList/ArrayList_Method.java
+++ src/Day30_ArrayList/ArrayList_Method.java
@@ -5,14 +5,47 @@ import java.util.ArrayList;
 public class ArrayList_Method {
     public static void main(String[] args) {
 
-        ArrayList<Integer> list1=new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
         list1.add(5);
         list1.add(7);
         list1.add(8);
-        list1.add(3,5);
-        list1.add(1,6);
+
+        list1.add(1, 6);
 
         System.out.println(list1);
+
+
+        /*
+        set(index,element): replaces the original one with new one
+         */
+
+        list1.set(3, 9);
+        System.out.println(list1);
+
+        System.out.println("==================================================================");
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.add(3);
+        list3.add(4);
+
+        int a = 1;
+
+        list3.remove(a);
+
+        System.out.println(list3);
+        System.out.println("==================================================================");
+
+        ArrayList<Integer> list4 = new ArrayList<>();
+        list4.add(10);
+        list4.add(20);
+        list4.add(30);
+        int b = 1;
+        list4.remove(b);
+        list4.remove(1);
+        System.out.println(list4);
+
 
     }
 }
---
+++ src/Day30_ArrayList/ArrayList_Methods2_Remove_Clear.java
@@ -0,0 +1,50 @@
+package Day30_ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList_Methods2_Remove_Clear {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+
+        //list.remove(1);
+        String str = "B";
+        list.remove(str);
+        boolean r1 = list.remove("F");
+        boolean r2 = list.remove("A");
+
+        System.out.println(list);
+
+        /*
+        clear(): it remove everything. Means size will be zero 0.
+
+         */
+
+        list.clear();
+        System.out.println(list);
+        System.out.println(list.size());
+
+        /*
+        indexOf(Object)>>>returns you its index number >>returns as an int
+         */
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+
+        int num1=list2.indexOf(1);
+        System.out.println(num1);
+
+        int num2=list2.indexOf(100);
+        System.out.println(num2); //gives -1 >>means it doesnt exist in this arraylist
+
+
+    }
+
+
+}
